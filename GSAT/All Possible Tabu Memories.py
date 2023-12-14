e650170b9ff1a7f57055f4090eddd135de450945
alexsimcock <ajsimcock@outlook.com>
2023-12-14 19:46:33 +0000

    Some changes to naming and structure better abstracted

--- GSAT/All Possible Tabu Memories.py
+++ GSAT/All Possible Tabu Memories.py
@@ -1,47 +1,17 @@
-def f1(x):
-    return not(x[0]) or not(x[1]) or not(x[2])
-    
-def f2(x):
-    return (x[1]) or (x[2])
-
-def f3(x):
-    return (x[1]) or not(x[3])
-
-def f4(x):
-    return not(x[1]) or not(x[2])
-    
-def f5(x):
-    return not(x[2]) or not(x[3])
-    
-def f6(x):
-    return not(x[1]) or (x[3])
-
-def f7(x):
-    return (x[2]) or (x[3])
-    
-def f8(x):
-    return (x[0]) or not(x[1]) or (x[2])
-    
-functions = [f1, f2, f3, f4, f5, f6, f7, f8]
-
-def f(x):        
-    return [int(function(x)) for function in functions]
-
-def solved_problems_heuristic(x):
-    return sum(f(x))
+from operator import attrgetter
 
 def get_neighbourhood(x, memory):
     neighbourhood = []
-    copy = x.copy()
+    copy = x.copy()    
     flippable_bits = [i for i in range(len(memory)) if memory[i] == 0]
-    
+
     neighbourhood = {index: tuple(copy[:index] + [int(not(copy[index]))] + copy[index + 1:]) for index in flippable_bits}
-    return {flip_index: {"x": flipped_value, "f": f(flipped_value), "h": solved_problems_heuristic(flipped_value)} for flip_index, flipped_value in neighbourhood.items()}
+    return {flip_index: {"x": flipped_value, "f": eval(flipped_value), "h": sum(eval(flipped_value))} for flip_index, flipped_value in neighbourhood.items()}
 
 class TabuNode():
     def __init__(self, x, memory):
         self.x = x
-        self.f_values = f(x)
+        self.f_values = eval(x)
         
         self.memory = memory
         
@@ -58,47 +28,55 @@ class TabuNode():
         
     def print_node(self):
         print(f"X: {self.x}\nMemory: {self.memory}\nF-Value: {sum(self.f_values)}\nF: {self.f_values}")
+        
+        
+functions = {"f1": lambda x: not(x[0]) or not(x[1]) or not(x[2]),
+             "f2": lambda x: (x[1]) or (x[2]),
+             "f3": lambda x: (x[1]) or not(x[3]),
+             "f4": lambda x: not(x[1]) or not(x[2]),
+             "f5": lambda x: not(x[2]) or not(x[3]),
+             "f6": lambda x: not(x[1]) or (x[3]),
+             "f7": lambda x: (x[2]) or (x[3]),
+             "f8": lambda x: (x[0]) or not(x[1]) or (x[2])}
+
+def eval(x):        
+    return {func_name: int(function(x)) for func_name, function in functions.items()}
+
+def unsolved_heuristic(x):
+    return len(eval(x)) - sum(eval(x))
 
 if __name__ == "__main__":
     # Initialise values
     start_value = [0, 1, 0, 1]
     memory = [0] * len(start_value)
     
-    # 1 Get neighbours who are not in memory
-    # 2 Generate their manhattan value
-        # Generate a dictionary with neighbours and manhattan value, for the purpose of output it would be nice to include f values
-    # 3 Choose best available. repeat
-    
     start = TabuNode(start_value, memory)
-    start.get_children()
     
-    print(start.x)
-    for flipped, child in start.children.items():
-        print(f"Flipping {flipped} gives {child.x}, f-value: {sum(child.f_values)}")
-    
-    # Expand only the best available ones...
-    
-    
-    print(f"X: {start_value}\nMemory: {memory}")
-    # 1
-    funcs = [("f" + str(i+1)) for i in range(len(functions))]
-    print("\n|Var|" + "|".join(funcs) + "|h-val|")
-    print("|-" * (len(start_value) + 6) + "|")
-    for flipped_bit, neighbour in get_neighbourhood(start_value, memory).items():
-        # 2
-        print("|" + "x" + str(flipped_bit + 1) + "|" + "|".join(map(str, map(int, neighbour["f"]))) + "|" + str(neighbour["h"]) + "|")
-        #full_dict = {flipped_bit: {"value": neighbour, "f": f(neighbour), "h": solved_problems_heuristic(neighbour)}}
-        #print(f"flipping bit {flipped_bit} gives: {neighbour}, heuristic value {solved_problems_heuristic(neighbour)}")
+    # Could be done through recursion? One parameter being max-recursion depth...
+    step_counter = 0
+    current_nodes = [start]
+    while step_counter < 3:
+        step_counter += 1
         
-    
-
-    # variables = ["x_" + str(k) for k in range(len(start_value))]
-    # functions_str = ["F_" + str(k) for k in range(len(functions))]
-    
-    # print("| " + " | ".join(variables) + " |")
-    # print("| --- "*len(start_value) + "|")
-    # for neighbour in get_neighbourhood(start_value):
-    #     print("| " + "  |  ".join(map(str, neighbour)) + " |")
-
-    
-    #print(solved_problems_heuristic(start_value))
+        # 1 Get neighbours who are not in memory
+        for node in current_nodes:
+            node.get_children()
+            
+            # 2 Generate their h value
+            for flipped, child in node.children.items():
+                print(f"Flipping {flipped} gives {child.x}, f-value: {sum(child.f_values)}")
+            
+            # 3 Find expandable nodes per node in current nodes...
+            max_f = max([sum(child.f_values) for child in node.children])
+            expandable_kids = [child for child in node.children if sum(child.f_values) == max_f]
+            
+            current_nodes.append(expandable_kids) ##
+        
+        #print(f"X: {start_value}\nMemory: {memory}")
+        # 1
+        #funcs = [("f" + str(i+1)) for i in range(len(functions))]
+        #print("\n|Var|" + "|".join(funcs) + "|h-val|")
+        #print("|-" * (len(start_value) + 6) + "|")
+        #for flipped_bit, neighbour in get_neighbourhood(start_value, memory).items():
+            # 2
+        #    print("|" + "x" + str(flipped_bit + 1) + "|" + "|".join(map(str, map(int, neighbour["f"]))) + "|" + str(neighbour["h"]) + "|")
